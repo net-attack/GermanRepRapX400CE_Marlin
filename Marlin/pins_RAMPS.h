6b12b98e534431cf4a15de08ceea197e0371a690
Philipp Wagner <philipp.wagner@vector.com>
2021-06-16 20:43:33 +0200

    âdd first running with wrong extruder direction

--- Marlin/pins_RAMPS.h
+++ Marlin/pins_RAMPS.h
@@ -73,7 +73,7 @@
 #define X_MAX_PIN         	-1
 #define Y_MIN_PIN          14
 #define Y_MAX_PIN          -1
-#define Z_MIN_PIN          19
+#define Z_MIN_PIN          18
 #define Z_MAX_PIN          -1
 
 
