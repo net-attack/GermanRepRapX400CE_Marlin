a652fcbf1c21619b0928ba348d666b81ca35ae3e
Philipp Wagner <philipp.wagner@vector.com>
2021-06-20 12:58:14 +0200

    update slicer and configuration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -595,7 +595,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 66.6666666666667, 66.6666666666667, 2409, 70}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 66.6666666666667, 66.6666666666667, 2409, 90}
 
 /**
  * Default Max Feed Rate (mm/s)
