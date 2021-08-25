ddd5f79254273cea7f30fc1cbdaee6901200d29e
Philipp Wagner <philipp.wagner@vector.com>
2021-08-25 22:15:55 +0200

    update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -595,7 +595,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 66.6666666666667, 66.6666666666667, 2409, 90}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 66.6666666666667, 66.6666666666667, 2409, 95}
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -645,7 +645,7 @@
  *
  * See https://github.com/synthetos/TinyG/wiki/Jerk-Controlled-Motion-Explained
  */
-//#define S_CURVE_ACCELERATION
+#define S_CURVE_ACCELERATION
 
 //===========================================================================
 //============================= Z Probe Options =============================
